4534449124140d53f17328fe6371e90f7d811f39
megha <megha.patel2@sheridancollege.ca>
2023-05-25 11:08:07 -0400

    error removed

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -16,8 +16,8 @@ public class DemoGit {
     public static void main(String[] args) {
         System.out.println("hello this is my first git operation");
         
-        int a;
-        int b;
+        int a=10;
+        int b=20;
         int c=a+b;
     }
     
