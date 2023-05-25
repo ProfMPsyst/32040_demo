967e11edce7256e30f1d40556e99008527a5c941
ProfMPsyst <65903834+ProfMPsyst@users.noreply.github.com>
2023-05-25 11:25:47 -0400

    Update DemoGit.java
    print sum at line 23

--- src/demogit/DemoGit.java
+++ src/demogit/DemoGit.java
@@ -19,7 +19,9 @@ public class DemoGit {
         int a=10;
        int b=20;
         int c=a+b;
-        int d=a+b+c;
+        //int d=a+b+c;
+        System.out.println("sum: "+c);
+        
     }
     
 }
