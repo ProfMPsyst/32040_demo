30ff8d3a930655c799a8c0905530fc85ae9e4ca1
megha <megha.patel2@sheridancollege.ca>
2023-06-01 09:27:17 -0400

    Person class added

---
+++ src/demogit/Person.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package demogit;
+
+/**
+ *
+ * @author Megha Patel,2023
+ */
+public class Person {
+    private String name;
+    private int SINNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSINNo() {
+        return SINNo;
+    }
+
+    public void setSINNo(int SINNo) {
+        this.SINNo = SINNo;
+    }
+    
+}
