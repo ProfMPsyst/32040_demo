d03b1b1b911206042818071c8d8c6209f9aec2dc
Prabhnoor <grprabhn@sheridancollege.ca>
2023-06-01 09:44:46 -0400

    abc added

---
+++ src/week_4.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 12898
+ */
+public class week_4 {
+     int a=1;
+    int b=2;
+    int c= a+b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+    
+}
+
+
