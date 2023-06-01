f729d7139ee304a87542473cf346daf2f61ceed9
vuongquocnguyen <nguvuong@sheridancollege.ca>
2023-06-01 09:28:48 -0400

    first commit the branch

---
+++ src/demogit/Person.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package demogit;
+
+/**
+ *
+ * @author Vuong Quoc Nguyen, 2023
+ */
+public class Person {
+    private String name;
+    private int SINNO;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSINNO() {
+        return SINNO;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSINNO(int SINNO) {
+        this.SINNO = SINNO;
+    }
+    
+    
+
+}
