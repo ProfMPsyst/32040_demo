108f9ff632718c2613932347e9d5429b7a7c1af8
Jennifer1524 <tailjenn@sheridancollege.ca>
2023-06-01 09:37:49 -0400

    Person class added

---
+++ src/demogit/Person.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package demogit;
+
+/**
+ *
+ * @author jenni
+ */
+public class Person {
+    int a;
+    int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    
+    
+}
