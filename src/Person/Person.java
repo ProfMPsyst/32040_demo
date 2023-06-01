097dc918f73051d10f8dfb4a61c7eafb3f21842b
Prabhpreet Kaur <kau15491@sheridancollege.ca>
2023-06-01 09:34:32 -0400

    Person class Added

---
+++ src/Person/Person.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.Person to edit this template
+ */
+
+package Person;
+
+/**
+ *
+ * @author Prabhpreet Kaur,2023
+ */
+public class Person {
+    String name;
+    int sinNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSinNo() {
+        return sinNo;
+    }
+
+    public void setSinNo(int sinNo) {
+        this.sinNo = sinNo;
+    }
+    
+    
+}
