bafe1042d11bc737af5574a49a9279c6caf056a9
saijasha <saijasha@sheridancollege.ca>
2023-06-01 09:36:17 -0400

    Student class

---
+++ src/demogit/Student.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package demogit;
+
+/**
+ *
+ * @author saijasha
+ */
+public class Student {
+    private String name;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
