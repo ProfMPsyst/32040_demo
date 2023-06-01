03c1cd5cc331410f5afae5a4652b582391972c7e
Laksh Kundnani <kundnanl@sheridancollege.ca>
2023-06-01 09:35:54 -0400

    Created Student and Testing class

---
+++ src/Testing.java
@@ -0,0 +1,6 @@
+public class Testing{
+    public static void main(String[] args) {
+        Student student = new Student(18,"Laksh Kundnani");
+        System.out.println(student.toString());
+    }
+}
